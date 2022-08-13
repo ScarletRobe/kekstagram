b895079b0c20a0c3c76be8402e4310427a9c2ad6
Leonid Chernyakov <scrltrb@gmail.com>
2022-08-13 17:50:31 +0300

    Fix: incorrect units for filter settings

--- js/new-publication/new-publication-render.js
+++ js/new-publication/new-publication-render.js
@@ -1,11 +1,26 @@
 import { renderThumbnails } from '../render-thumbnails.js';
 
 const effects = {
-  chrome: 'grayscale',
-  sepia: 'sepia',
-  marvin: 'invert',
-  phobos: 'blur',
-  heat: 'brightness'
+  chrome: {
+    effect: 'grayscale',
+    unit: ''
+  },
+  sepia: {
+    effect: 'sepia',
+    unit: ''
+  },
+  marvin: {
+    effect: 'invert',
+    unit: '%'
+  },
+  phobos: {
+    effect: 'blur',
+    unit: 'px'
+  },
+  heat: {
+    effect: 'brightness',
+    unit: ''
+  }
 };
 
 const hashtagsInputElement = document.querySelector('.text__hashtags');
@@ -19,12 +34,12 @@ const applyEffects = (formData, id) => {
   const effectName = formData.get('effect');
   const effectLevel = formData.get('effect-chrome') || formData.get('effect-sepia') || formData.get('effect-marvin') || formData.get('effect-phobos') || formData.get('effect-heat');
   const newPhoto = document.querySelector(`.picture[data-id="${id}"] img`);
-  newPhoto.style.filter = `${effects[effectName]}(${effectLevel})`;
-  newPhoto.style.transform = `scale(${scale})`;
+  newPhoto.style.filter = `${effects[effectName].effect}(${effectLevel}${effects[effectName].unit})`;
+  newPhoto.style.transform = `scale(${scale}%)`;
 
   return {
-    'filter': `${effects[effectName]}(${effectLevel})`,
-    'transform': `scale(${scale})`
+    'filter': `${effects[effectName].effect}(${effectLevel}${effects[effectName].unit})`,
+    'transform': `scale(${scale}%)`
   };
 };
 
